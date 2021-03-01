0c31c05ee738b6eb5f68696bd498218889c6eabc
Gustavo <Gustavo@LAPTOP-O6D53KO9>
2021-03-01 03:07:55 -0300

    Incremento dos metodos post e junção do get

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App () {
+  //Estados da Rota Get
   const [nome, setNome] = useState();
   const [idade, setIdade] = useState();
   const [message, setMessage] = useState();
 
+  //Estados da rota Post
+  const [sobrenome, setSobrenome] = useState();
+  const [idade2, setIdade2] = useState();
+  const [message2, setMessage2] = useState();
+
+  //Estados da rota Post
+  const [nome1, setNome1] = useState();
+  const [idade1, setIdade1] = useState();
+  const [message1, setMessage1] = useState();
+
+  //Função que executa a rota Get
   async function chama(){
     const response = await fetch('/api/mensagem');
     const body = await response.json();
@@ -17,16 +27,56 @@ function App () {
     setMessage(body.message);
   };
 
-  chama();
+  //Implementação do metodo post de requisição da api
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' }
+};
+
+async function sobre(){
+  const response = await fetch('/api/mensagem', requestOptions);
+  const body = await response.json();
+  if (response.status !== 200) throw Error(body.message);
+
+  setSobrenome(body.sobrenome);
+  setIdade2(body.idade);
+  setMessage2(body.message);
+};
+  //Função que executa a rota Post
+  async function chama1(){
+    const response = await fetch('/api/mensagem1', requestOptions);
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+  
+    setNome1(body.nome);
+    setIdade1(body.idade);
+    setMessage1(body.message);
+  };
+
+  //Executando a rotas
+  chama();//rota get
+  sobre();
+  chama1();// diferente rota post
+
+  //As duas rotas poderiam ser as mesmas com metodos de semantica diferentes 
+  //EX: fetch('/api/mensagem'); fetch('/api/mensagem', requestOptions);;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">Nome: {nome}</p>
-      <p className="App-intro">Idade: {idade}</p>
-      <p className="App-intro">Message: {message}</p>
+      <h1>Rota Get + Rota Post</h1>
+      {/*Exibindo a rota get*/}
+      {/*Os parametros das duas rotas podem ser diferentes pois há duas rotas para a mesma rota basicamente, a rota Post com seus parametros
+      e a rota Get com seus parametros que podem ser totalmente diferentes e numericamente diferentes */}
+      <p className="App-intro">Nome: {nome}// Sobrenome: {sobrenome}</p>{/*implementando a rota Post junto a get do mesmo caminho */}
+      <p className="App-intro">Idade: {idade}// Idade2 :{idade2}</p>
+      <p className="App-intro">Message: {message}// Message2: {message2}</p>
+      
+    <br></br>
+
+      <h1>Rota Post apenas</h1>
+      {/*Exibindo a rota post*/}
+      <p className="App-intro">Nome: {nome1}</p>
+      <p className="App-intro">Idade: {idade1}</p>
+      <p className="App-intro">Message: {message1}</p>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+//Rota get, rota menos segura porém mais agil pra carregar, ou seja custa menos processamento
 app.get('/api/mensagem', (req, res) => {
-  res.send({ nome: 'Gustavinho', idade: '16', message:'Gustavinho que enviou' });
+  res.send({ nome: 'Gustavo', idade: '18', message:'Gustavo esta enviando esssa mensagem' });
+});
+//Rota post, rota mais segura porém demora alguns milesimos a mais pra carregar, ou seja custa mais processamento
+app.post('/api/mensagem', (req, res) => {
+  res.send({ sobrenome: 'Almeida', idade: '12', message:'Pedro enviou uma maçã' });
+});
+
+//Rota post, rota mais segura porém demora alguns milesimos a mais pra carregar, ou seja custa mais processamento
+app.post('/api/mensagem1', (req, res) => {
+  res.send({ nome: 'Pedro', idade: '23', message:'Pedro enviou um relatorio' });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
